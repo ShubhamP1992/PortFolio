71405406315f716a49e36e42a95955f60b4e2dd2
SHUBHAMP1992 <panchalshubham1992@gmail.com>
2025-06-02 21:58:46 +0530

    added the links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Typewriter } from 'react-simple-typewriter';
 import './Hero.css';
 import heroImage from './hero.png';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function Hero() {
   return (
@@ -23,6 +24,17 @@ function Hero() {
         </h1>
         <p>I'm a Developer who loves building modern web apps.</p>
         <a href="#contact" className="cta-button">Get In Touch</a>
+        
+
+        <div className="social-links">
+          <a href="https://www.linkedin.com/in/shubhampanchal1992/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin size={24} color="#0a66c2" />
+          </a>
+          <a href="https://github.com/ShubhamP1992" target="_blank" rel="noopener noreferrer">
+            <FaGithub size={24} color="#000" />
+          </a>
+        </div>
+
       </div>
 
       <div class="hero-right"></div>
