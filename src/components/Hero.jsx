937ea39ec3648dbda43a26f7d133215b0a70be47
SHUBHAMP1992 <panchalshubham1992@gmail.com>
2025-06-02 20:48:58 +0530

    Ui update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Typewriter } from 'react-simple-typewriter';
 import './Hero.css';
+import heroImage from './hero.png';
 
 function Hero() {
   return (
-    <section id="hero" className="hero" data-aos="fade-up">
+    <section id="hero" className="hero">
       <div className="hero-content">
         <h1>
           Hi, I'm{' '}
-          <span style={{ color: '#00ADB5', fontWeight: 'bold' }}>
+          <span>
             <Typewriter
               words={['Shubham Panchal', 'Software Developer', '.NET Core Developer']}
-              loop={0} // 0 = infinite
+              loop={0}
               cursor
               cursorStyle="|"
               typeSpeed={70}
@@ -23,6 +24,10 @@ function Hero() {
         <p>I'm a Developer who loves building modern web apps.</p>
         <a href="#contact" className="cta-button">Get In Touch</a>
       </div>
+
+      <div class="hero-right"></div>
+
+      <img src={heroImage}  alt="Hero" className="hero-image" />
     </section>
   );
 }
